8cdf8a0e0a7d6bed2b353f516295d34d73573f7d
Yaron de Leeuw <me@jarondl.net>
2025-08-09 11:08:11 +0300

    Separate out ipv6 and ipv4 hosts, because of my usecase.

--- dedyn_curler.sh
+++ dedyn_curler.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-ip="$(curl -s --fail-with-body https://api.ipify.org)"
+# We accept a systemd template parameter, which will
+# decide both the hostname and proto. Yes, this is very specific,
+# but that's what I need in my case. If the hostname starts with
+# ipv4, it will get ipv4, and if with ipv6, then ipv6.
+hostname="$1"
+if [[ -z "$hostname" ]] || [[ ! "$hostname" =~ ^(ipv4|ipv6) ]] ; then
+  echo "hostname not specified, or wrong, ${hostname}" >&2
+  exit 1
+fi
+
+echo "starting dedyn curler for ${hostname}"
+
+case "$hostname" in
+  ipv4*)
+    ip="$(curl -s --fail-with-body https://api.ipify.org)"
+    ;;
+  ipv6*)
+    ip="$(curl -s --fail-with-body https://api6.ipify.org)"
+    ;;
+esac
 if (( $? != 0 )) || [[ -z "$ip" ]]; then
-  echo "Did not find ipv4" >&2
+  echo "Did not find ip" >&2
   exit 1
 fi
 
-ipv6="$(curl -s --fail-with-body https://api6.ipify.org)"
-if (( $? != 0 )) || [[ -z "$ipv6" ]]; then
-  echo "Did not find ipv6" >&2
-  exit 1
-fi
+req="https://update.dedyn.io/?myip=${ip}&hostname=${hostname}"
+echo "requesting curl ${req}"
 
-curl -s --fail-with-body -K "${CREDENTIALS_DIRECTORY}/dedyn_user" "https://update.dedyn.io/?myipv4=${ip}&myipv6=${ipv6}"
+curl -s --fail-with-body -K "${CREDENTIALS_DIRECTORY}/dedyn_user" "${req}"
 if (( $? != 0 )) then
   echo "Update failed" >&2
   exit 1
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 
-install -m 0644 dedyn_curler.{service,timer} /etc/systemd/system/ &&
-	install dedyn_curler.sh /usr/local/bin/
+install -m 0644 dedyn_curler@.{service,timer} /etc/systemd/system/ &&
+	install dedyn_curler.sh /usr/local/bin/ &&
+  systemctl daemon-reload
 
